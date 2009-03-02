8ec437bb645101bfd7440e9e641ca1f6b355cecd
Guillaume Nargeot <guillaume@guillaume-linux.(none)>
2009-03-02 15:41:57 +0900

    removed a useless test

--- 009.rb
+++ 009.rb
@@ -5,11 +5,9 @@
 	(a + 1).upto(1000) do |b|
 		c = 1000 - a - b
 		break if c < b
-		if a + b + c == 1000
-			if a ** 2 + b ** 2 == c ** 2
-				puts a * b * c
-				exit
-			end
+		if a ** 2 + b ** 2 == c ** 2
+			puts a * b * c
+			exit
 		end
 	end
 end
