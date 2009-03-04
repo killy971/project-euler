9cb93eef22e9eb165adb826d25851994c69e40e0
Guillaume Nargeot <guillaume@guillaume-linux.(none)>
2009-03-04 09:42:01 +0900

    make use of 'use'

--- 024.clj
+++ 024.clj
@@ -1,7 +1,7 @@
-(require 'clojure.contrib.combinatorics)
+(use 'clojure.contrib.combinatorics)
 
 (defn nth-permutation [n]
-  (nth (clojure.contrib.combinatorics/permutations (range 10)) (dec n)))
+  (nth (permutations (range 10)) (dec n)))
 
 (println
   (reduce str (nth-permutation 1000000)))
