fe26d9e3ed18765218813584dcefecff3d2384ac
Guillaume Nargeot <killy971@gnargeot.(none)>
2009-03-06 02:35:25 +0000

    added the clojure solution for problem 92

---
+++ 092.clj
@@ -0,0 +1,14 @@
+(defn digits [x]
+  (loop [n x, dseq []]
+    (if (< n 10)
+      (cons n dseq)
+      (recur (quot n 10) (cons (rem n 10) dseq)))))
+
+(defn ends-by [n]
+  (loop [i n]
+    (let [next (reduce + (map #(* % %) (digits i)))]
+      (if (or (= next 1) (= next 89))
+        next
+        (recur next)))))
+
+(println (count (filter #(= % 89) (map ends-by (range 1 9999999)))))
