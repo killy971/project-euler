20d7dfa8ac9a5159898bc8d6004e99b7dd0b24c1
Guillaume Nargeot <guillaume@guillaume-linux.(none)>
2009-03-19 15:34:12 +0900

    added clojure solution for problem 71

--- 071.clj
+++ 071.clj
@@ -1,5 +1,10 @@
-(defn closest [numer denom]
+(defn closest-fraction-numerator [numer denom max-denom]
   (let [upper (/ numer denom)]
-    (loop [n numer, d (inc denom), closest_n 0, closest_f 0]
+    (loop [n numer, d (inc denom), c-n 0, c-f 0]
       (cond
-        (< 
+        (= max-denom d) c-n
+        (< (/ (inc n) d) upper) (recur (inc n) d c-n c-f)
+        :default (let [f (/ n d) closer (> f c-f)]
+                   (recur n (inc d) (if closer n c-n) (if closer f c-f)))))))
+
+(println (closest-fraction-numerator 3 7 1000000))
