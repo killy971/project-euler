7afd043147d1ac6013803c080d0af7913967620e
Guillaume Nargeot <killy971@ahiru.local>
2009-03-20 17:36:38 +0900

    added an explanation note

--- 041.clj
+++ 041.clj
@@ -13,3 +13,5 @@
       (= d-list (range 1 (inc (count d-list)))))))
 
 (println (last (filter #(pandigital? %) (sieve 7654321))))
+
+; cannot try with numbers containing 8 or 9, because they can't be prime
