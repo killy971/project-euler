03e48f9475e1b500a5c47bd410180086ccfb55b7
Guillaume Nargeot <killy971@ahiru.local>
2009-04-18 12:22:48 +0900

    added erlang solution for problem 6

---
+++ 006.erl
@@ -0,0 +1,8 @@
+#!/opt/local/bin/escript
+% vim:set ts=4 sw=4:
+
+main(_) ->
+	Square = fun(X) -> ( X * X ) end,
+	SquareSum = lists:sum(lists:map(Square, lists:seq(1, 100))),
+	SumSquare = Square(lists:sum(lists:seq(1, 100))),
+	io:format("~w~n", [SumSquare - SquareSum]).
