d6b333dcc677d2697efc84799191e3024dd841d0
Nargeot Guillaume <killy971@ahiru.local>
2009-05-12 22:44:44 +0900

    refactoring

--- 041.clj
+++ 041.clj
@@ -9,9 +9,9 @@
 (defn pandigital? [x]
   (let [d-list (sort (digits x))]
     (and
-      (not= 0 (first d-list))
+      (< 0 (first d-list))
       (= d-list (range 1 (inc (count d-list)))))))
 
-(println (last (filter #(pandigital? %) (sieve 7654321))))
+(println (last (filter pandigital? (sieve 7654321))))
 
 ; cannot try with numbers containing 8 or 9, because they can't be prime
