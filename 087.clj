db5a3f6886a685ccecce1a2ec56170606028a5f4
Guillaume Nargeot <killy971@gmail.com>
2009-09-19 23:18:09 +0900

    improved performance of clojure solution to problem 87

--- 087.clj
+++ 087.clj
@@ -10,22 +10,19 @@
 
 (defn for-c [primes, max-n, p-sum, array]
   (for-p primes max-n p-sum array #(* % (* % (* % %)))
-            (fn [sum, arr] (do (aset-int arr sum 1) arr))))
+         (fn [sum, arr] (do (aset-int arr sum 1) arr))))
 
 (defn for-b [primes, max-n, p-sum, array]
   (for-p primes max-n p-sum array #(* % (* % %))
-            (fn [sum, arr] (for-c primes max-n sum arr))))
+         (fn [sum, arr] (for-c primes max-n sum arr))))
 
 (defn for-a [primes, max-n, p-sum, array]
   (for-p primes max-n p-sum array #(* % %)
-            (fn [sum, arr] (for-b primes max-n sum arr))))
+         (fn [sum, arr] (for-b primes max-n sum arr))))
 
 (defn fill-array [max-n]
-  (let [array (int-array max-n 0)
-        primes (sieve (Math/floor (Math/sqrt (- max-n (* 2 2) (* 2 2 2)))))]
+  (let [array (int-array max-n)
+        primes (sieve (Math/sqrt (- max-n (* 2 2) (* 2 2 2))))]
     (for-a primes max-n 0 array)))
 
-(defn count-ones [array]
-  (count (filter #(= % 1) array)))
-
-(println (count-ones (fill-array 5e7)))
+(println (reduce + (fill-array 5e7)))
